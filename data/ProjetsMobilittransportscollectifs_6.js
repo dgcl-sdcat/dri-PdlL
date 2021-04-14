532f61418382cd0ab10196bcd75ab5ecca2be2f2
dgcl-sdcat <80058755+dgcl-sdcat@users.noreply.github.com>
2021-04-14 15:19:56 +0200

    Add files via upload

---
+++ data/ProjetsMobilittransportscollectifs_6.js
@@ -0,0 +1,9 @@
+var json_ProjetsMobilittransportscollectifs_6 = {
+"type": "FeatureCollection",
+"name": "ProjetsMobilittransportscollectifs_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "CONSTRUCTION D’UN NOUVEAU FERRY POUR ASSURER LA DESSERTE DE L’ILE D’YEU", "Coût": "21 890 000 €", "Subvention": "8 099 300 €" }, "geometry": { "type": "Point", "coordinates": [ -2.347822035, 46.7258633 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "DEMENAGEMENT DU SITE SNCF NANTES ETAT A NANTES BLOTTEREAU – financement des surcoûts du chantier", "Coût": "900 000 €", "Subvention": "720 000 €" }, "geometry": { "type": "Point", "coordinates": [ -1.511851, 47.230805 ] } }
+]
+}
---
+++ data/Projetsautresthmatique_4.js
@@ -0,0 +1,9 @@
+var json_Projetsautresthmatique_4 = {
+"type": "FeatureCollection",
+"name": "Projetsautresthmatique_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thème": "Autres", "Projet": "LYCEE D’AIZENAY – CONSTRUCTION NEUVE", "Coût": "32 980 373 €", "Subvention": "11 817 967 €" }, "geometry": { "type": "Point", "coordinates": [ -1.545679, 46.725508 ] } },
+{ "type": "Feature", "properties": { "Thème": "Autres", "Projet": "COLD IRONING SUR LES QUAIS DE PORT FROMENTINE ET DE PORT JOINVILLE", "Coût": "800 000 €", "Subvention": "520 000 €" }, "geometry": { "type": "Point", "coordinates": [ -2.345132264, 46.72517199 ] } }
+]
+}
